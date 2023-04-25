4652f37cbedc79167e71fefeb19901862202214c
Veteread <131723583+Veteread@users.noreply.github.com>
2023-04-25 21:31:41 +0300

    Add files via upload

---
+++ PracticeCSh1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+//  Exercise 1
+//Console.WriteLine("Hello World!!!");
+//Console.ReadKey();
+//
+//  Exercise 2
+Console.Write("Hello ");
+Console.Write("World");
+Console.Write("!!!");
+Console.ReadLine();
